dd48294c5a5c8e0c37c0d5b1e8ab08644712d237
Lyline <greg.quesne@gmail.com>
2022-05-03 10:48:26 +0200

    Configuration project Spring, jaCoCo

---
+++ src/main/java/com/medicscreen/diagnosticmicroservice/DiagnosticMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.medicscreen.diagnosticmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagnosticMicroserviceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DiagnosticMicroserviceApplication.class, args);
+  }
+
+}
